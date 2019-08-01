e05c5b01139f93d606c6f876319d92e3f91fa519
Aaron Baker <aaronbaker9210@gmail.com>
2019-07-31 22:23:23 -0500

    chore: add manual build

---
+++ precache-manifest.8b8d971357d3064d5c2b598226262f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e7be2e2c5c4e8176fb0ea222ff6a23",
+    "url": "/things-i-learn/index.html"
+  },
+  {
+    "revision": "271bfb414863f34d5ca6",
+    "url": "/things-i-learn/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/things-i-learn/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "271bfb414863f34d5ca6",
+    "url": "/things-i-learn/static/js/main.cff8dcf4.chunk.js"
+  },
+  {
+    "revision": "da10c3ee8b1ec6b62953",
+    "url": "/things-i-learn/static/js/runtime~main.6a6ff6aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/things-i-learn/static/media/logo.5d5d9eef.svg"
+  }
+]);
